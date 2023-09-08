7f1ab74b0ff5b37ac86138c48412ed2718cef8d8
Juanjo <110014245+juanjo-alvarez@users.noreply.github.com>
2023-09-08 08:07:56 +0200

    Ejemplo Alimentación nombre

--- src/main/java/com/campusdual/ejemplos/alimentos/FoodUtility.java
+++ src/main/java/com/campusdual/ejemplos/alimentos/FoodUtility.java
@@ -2,7 +2,7 @@ package com.campusdual.ejemplos.alimentos;
 
 public class FoodUtility {
     public static void main(String[] args) {
-        Food zanahoria = new Food(12,0,1);
-        System.out.println("Calorias de 100g de Zanahoria:"+zanahoria.getCalories(100));
+        Food zanahoria = new Food("Zanahoria",12,0,1);
+        System.out.println("Calorias de 100g de "+zanahoria.getName()+":"+zanahoria.getCalories(100));
     }
 }
